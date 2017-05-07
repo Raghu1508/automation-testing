0351b343c4276f9b70a3cee1724b9d18145f65c7
ankitr.nalkv <ankitr.nalkv@gmail.com>
2017-05-07 15:05:08 +0530

    Added java program

---
+++ Java Prog/src/demoProg.java
@@ -0,0 +1,16 @@
+import javax.swing.text.AttributeSet.CharacterAttribute;
+
+public class demoProg {
+
+	public static void main(String[] args)
+	{
+		String s1 = "Character";
+		String s2="";
+		for(int i=s1.length()-1;i>=0;i--)
+		{
+			s2=s2+s1.charAt(i);
+		}
+			
+		System.out.println(s2);
+	}
+}
